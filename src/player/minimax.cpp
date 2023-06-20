e4b79508594a52d3608a428b7512144ab615b86e
Baoj455 <jeter455345@gmail.com>
2023-06-20 21:58:50 +0800

    final final version

--- src/player/minimax.cpp
+++ src/player/minimax.cpp
@@ -34,8 +34,12 @@ void read_board(std::ifstream& fin) {
 
 void write_valid_spot(std::ofstream& fout) {
   // Keep updating the output until getting killed.
+  int depth = 0;
   while(true) {
-    auto move = Minimax::get_move(root, 4);
+    auto move = Minimax::get_move(root, 5);
+    depth++;
+    if(depth >= 4)
+      depth = 0;
     fout << move.first.first << " " << move.first.second << " "\
          << move.second.first << " " << move.second.second << std::endl;
     
